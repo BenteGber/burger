164360b9761b5fce18d31dfc4e751e7bd77a49bc
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-04-05 14:57:55 -0400

    configured jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: process.env || 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
