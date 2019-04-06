4c0ffadabdb664e497d29a1aa7ebd9ec291ed095
Bente Gber <benjamingreenberg@Benjamins-MBP-2.attlocal.net>
2019-04-06 17:54:46 -0400

    added JAWS_DB config and dotenv for dev

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # burger
+
+User enters a burger they would like to eat 
+
+and Then it moves the burger into
--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,26 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+let connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: process.env || 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    console.log('JAWS')
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    console.log("local")
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port: process.env.PORT || 3306,
+    user: process.env.MYSQLDEVUSRNAME,
+    password: process.env.MYSQLDEVPWD,
+    database: 'burgers_db',
+  });
+}
 
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
